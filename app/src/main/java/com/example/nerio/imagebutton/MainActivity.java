35ef93cb56fd63e4f4535e064debf5278f56c051
Nerio <neriogonzalez@gmail.com>
2018-07-15 22:57:22 -0400

    Initial commit

---
+++ app/src/main/java/com/example/nerio/imagebutton/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.nerio.imagebutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void MensajeGitHub(View vista) {
+        Toast.makeText(this, "Github", Toast.LENGTH_SHORT).show();
+        
+    }
+
+}
